0f04cc856e264dd690ecdc3f508d92f90315b145
Simon Ding <Simon@SD_PC>
2014-04-09 20:49:44 +0200

    Fixed error

--- src/de/simonding/yaces/EventSource.java
+++ src/de/simonding/yaces/EventSource.java
@@ -1,8 +1,5 @@
 package de.simonding.yaces;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -10,7 +7,7 @@ import java.util.Vector;
 
 
 public class EventSource {
-	private HashMap<String, List<EventListener<? extends Event>>> events = new HashMap<>();
+	private HashMap<String, List<EventListener<Event>>> events = new HashMap<>();
 	
 	public synchronized <T extends Event> void addEventListener(EventListener<T> listener, Class<T> eventClass) {
 		
@@ -18,12 +15,12 @@ public class EventSource {
 		
 		//Create List if not done already
 		if(!events.containsKey(key)){
-			List<EventListener<? extends Event>> value = new Vector<>();
+			List<EventListener<Event>> value = new Vector<>();
 			events.put(key, value);
 		}
 		
 		//Add listener to List
-		events.get(key).add(listener);
+		events.get(key).add((EventListener<Event>) listener);
 	}
 	
 	public synchronized <T extends Event> void removeListener(EventListener<T> listener, Class<T> eventClass) {
@@ -41,7 +38,7 @@ public class EventSource {
 	public synchronized <T extends Event> void fireEvent(T event){
 		String key = event.getClass().getName();
 		if(events.containsKey(key)){
-			Iterator<EventListener<? extends Event>> i = events.get(key).iterator();
+			Iterator<EventListener<Event>> i = events.get(key).iterator();
 			while(i.hasNext()){
 				
 				i.next().receiveEvent(event); //Compiler Error
